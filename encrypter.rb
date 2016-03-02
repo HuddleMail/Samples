9ac502077c445ba32c30b6b30cd6db9844b04118
Neal Friedman <nfriedma@msudenver.edu>
2016-03-02 16:39:36 -0700

    working on the script...

--- encrypter.rb
+++ encrypter.rb
@@ -3,41 +3,15 @@
 require 'openpgp'
 require 'mail'
 
-$filename = "encrypted"
+def encrypterTest
+  message = ARGF.read
+  encrypted = `#$message | gpg -e -r "nealio"`
+  `#$encrypted | mail -s "ENCRYPTED" nealiof1000@gmail.com`
+  decrypted = `#$encrypted | gpg -d`
+  `#$decrypted | mail -s "decrypted" nealiof1000@gmail.com`
 
-def createFile
-  aFile = File.new("encrypted", "w+")
-  text = ARGF.read
-  puts "Unencrypted message: #{text}"
-  if aFile
-    aFile.syswrite(text)
-    aFile.close
-  else
-    puts "Unable to open file!"
-  end
 end
 
-def encrypt
-   #text = ARGF.read
-   text = `ARGF.read | gpg --encrypt -r "nealio" | mail -s "encrypted message" nealiof1000@gmail.com`
-   `text | mail -s "unencrypted text" nealiof1000@gmail.com"
-  #`rm #$filename.gpg`
-  #`gpg -e -r "nealio" #$filename`
-   #puts "The encrypted message:"
-   #puts `cat #$filename.gpg` 
-  
-end
+encrypterTest
 
-def decrypt
-  puts `gpg -d #$filename.gpg`
-end
-
-def sendMail
-  `mail    
- 
-end
-
-#createFile
-encrypt
-#decrypt
 
