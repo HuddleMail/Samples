2c43c7bdfb9be583d1ea308ed7d551485d2fcee9
Neal Friedman <nfriedma@msudenver.edu>
2016-03-02 17:30:49 -0700

    I think this one is good

--- encrypter.rb
+++ encrypter.rb
@@ -5,11 +5,10 @@ require 'mail'
 
 def encrypterTest
   message = ARGF.read
-  encrypted = `#$message | gpg -e -r "nealio"`
-  `#$encrypted | mail -s "ENCRYPTED" nealiof1000@gmail.com`
-  decrypted = `#$encrypted | gpg -d`
-  `#$decrypted | mail -s "decrypted" nealiof1000@gmail.com`
-
+  encrypted = `echo "#$message" | gpg -e -r "nealio"`
+  `echo "#$encrypted" | mail -s "ENCRYPTED" nealiof1000@gmail.com`
+  decrypted = `echo "#$encrypted" | gpg -d`
+  `echo "#$decrypted" | mail -s "DECRYPTED" nealiof1000@gmail.com`
 end
 
 encrypterTest
